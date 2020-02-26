e415a2b51f0c0108edae1f17f89d4bedc7efe9a6
ChrisKnowsDev <chrisharley81@gmail.com>
2020-02-26 16:44:29 -0500

    additional styling logo, close nav on link click

--- dist/js/index.js
+++ dist/js/index.js
@@ -4,3 +4,8 @@ const toggle = document.querySelector('.toggle');
 toggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 })
+
+// close nav on link click
+function closeNav() {
+  navLinks.classList.toggle('active');
+}
